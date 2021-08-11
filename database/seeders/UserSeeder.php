41566a9e36485fa2a07b1ce9fd62e45c6b90d2eb
TurhanCetin <turhancetin30@gmail.com>
2021-08-11 17:51:03 +0300

    ikinci gönderme

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,9 @@ class UserSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         \DB::table('users')->insert([
-            'name' => 'Emre',
+            'name' => 'Turhan',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'email' => 'mail@emredipi.com'
+            'email' => 'turhancetin30@gmail.com'
         ]);
     }
 }
